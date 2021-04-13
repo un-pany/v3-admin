4e8593f5a6a8c4c9723dc4bfe5c02ac8fb81993f
pany <pany@un-net.com>
2021-04-13 17:17:39 +0800

    style: 统一文件名

--- src/layout/components/index.ts
+++ src/layout/components/index.ts
@@ -6,7 +6,7 @@
  * @LastEditTime: 2020-12-24 10:40:32
  */
 export { default as AppMain } from './AppMain.vue'
-export { default as Navbar } from './navigation_bar/Index.vue'
+export { default as Navbar } from './navigation-bar/Index.vue'
 export { default as Settings } from './settings/Index.vue'
-export { default as Sidebar } from './side_bar/Index.vue'
-export { default as TagsView } from './tags_view/Index.vue'
+export { default as Sidebar } from './side-bar/Index.vue'
+export { default as TagsView } from './tags-view/Index.vue'
