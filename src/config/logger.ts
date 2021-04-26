fc4617d3827a902bf44a0b0bac8981a04f335719
pany <pany@un-net.com>
2021-04-26 15:51:20 +0800

    feat: logger全局控制开关

---
+++ src/config/logger.ts
@@ -0,0 +1,12 @@
+/* logger */
+
+interface LoggerSettings {
+  // 是否打开记录器（@/store/index）
+  openLogger: boolean
+}
+
+const loggerSettings: LoggerSettings = {
+  openLogger: false
+}
+
+export default loggerSettings
--- src/store/index.ts
+++ src/store/index.ts
@@ -4,6 +4,7 @@ import { store as settings, SettingStore, SettingsState } from '@/store/modules/
 import { store as permission, PermissionStore, PermissionState } from '@/store/modules/permission'
 import { store as user, UserStore, UserState } from '@/store/modules/user'
 import { store as tagViews, TagsStore, TagsViewState } from '@/store/modules/tagsview'
+import loggerSettings from '@/config/logger'
 
 export interface RootState {
     app: AppState
@@ -18,8 +19,12 @@ export type Store = AppStore<Pick<RootState, 'app'>> & SettingStore<Pick<RootSta
 & TagsStore<Pick<RootState, 'tagViews'>>
 
 // 在开发环境中插入记录器 logger
-const debug = process.env.NODE_ENV !== 'production'
-const plugins = debug ? [createLogger({})] : []
+let debug
+let plugins
+if (loggerSettings.openLogger) {
+  debug = process.env.NODE_ENV !== 'production'
+  plugins = debug ? [createLogger({})] : []
+}
 
 export const store = createStore({
   plugins,
