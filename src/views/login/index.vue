8f6b11235ff0806e698fd8a57f284261ea6276bc
pany <pany@un-net.com>
2021-04-30 13:37:46 +0800

    refactor: 登录页UI

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,69 +1,66 @@
 <template>
   <div class="login-container">
-    <el-form ref="loginFormDom" :model="loginForm" :rules="loginRules" class="login-form" auto-complete="on" label-position="left">
-      <div class="title-container">
-        <h3 class="title">
-          V3 - Admin
-        </h3>
+    <div class="login-card">
+      <div class="title">
+        <h3>V3 - Admin</h3>
       </div>
-
-      <el-form-item prop="username">
-        <span class="svg-container">
-          <svg-icon name="user" />
-        </span>
-        <el-input
-          ref="userNameDom"
-          v-model="loginForm.username"
-          placeholder="用户名"
-          name="username"
-          type="text"
-          tabindex="1"
-          auto-complete="off"
-        />
-      </el-form-item>
-
-      <el-form-item prop="password">
-        <span class="svg-container">
-          <svg-icon name="password" />
-        </span>
-        <el-input
-          :key="passwordType"
-          ref="passwordDom"
-          v-model="loginForm.password"
-          :type="passwordType"
-          placeholder="密码"
-          name="password"
-          tabindex="2"
-          auto-complete="off"
-        />
-        <!-- @keyup.enter.native="handleLogin" -->
-        <span class="show-pwd" @click="showPwd">
-          <svg-icon :name="passwordType === 'password' ? 'eye' : 'eye-open'" />
-        </span>
-      </el-form-item>
-
-      <el-form-item prop="code" style="width:330px;">
-        <span class="svg-container">
-          <svg-icon name="table" />
-        </span>
-        <el-input
-          ref="codeDom"
-          v-model="loginForm.code"
-          placeholder="验证码"
-          name="code"
-          type="text"
-          tabindex="3"
-          auto-complete="off"
-        />
-        <span class="show-code">
-          <img style="width: 100px; height: 52px;background: #f4def6;border-radius: 4px;" :src="src" alt="验证码" @click="createCode">
-        </span>
-      </el-form-item>
-
-      <el-button :loading="loading" type="primary" style="width:100%;margin-bottom:30px;" @click.prevent="handleLogin">
-        登录
-      </el-button>
-    </el-form>
+      <div class="content">
+        <el-form ref="loginFormDom" :model="loginForm" :rules="loginRules" auto-complete="on" label-position="left">
+          <el-form-item prop="username">
+            <span class="svg-container">
+              <svg-icon name="user" />
+            </span>
+            <el-input
+              ref="userNameDom"
+              v-model="loginForm.username"
+              placeholder="用户名"
+              name="username"
+              type="text"
+              tabindex="1"
+              auto-complete="off"
+            />
+          </el-form-item>
+          <el-form-item prop="password">
+            <span class="svg-container">
+              <svg-icon name="password" />
+            </span>
+            <el-input
+              :key="passwordType"
+              ref="passwordDom"
+              v-model="loginForm.password"
+              :type="passwordType"
+              placeholder="密码"
+              name="password"
+              tabindex="2"
+              auto-complete="off"
+            />
+            <span class="show-pwd" @click="showPwd">
+              <svg-icon :name="passwordType === 'password' ? 'eye' : 'eye-open'" />
+            </span>
+          </el-form-item>
+          <el-form-item prop="code">
+            <span class="svg-container">
+              <svg-icon name="table" />
+            </span>
+            <el-input
+              ref="codeDom"
+              v-model="loginForm.code"
+              placeholder="验证码"
+              name="code"
+              type="text"
+              tabindex="3"
+              auto-complete="off"
+            />
+            <span class="show-code">
+              <img style="width: 100px; height: 52px;background: #f4def6;border-radius: 4px;" :src="src" alt="验证码" @click="createCode">
+            </span>
+          </el-form-item>
+          <el-button :loading="loading" type="primary" @click.prevent="handleLogin">
+            登录
+          </el-button>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -219,11 +216,66 @@ export default defineComponent({
 </script>
 
 <style lang="scss">
+.login-container {
+  min-height: 100%;
+  width: 100%;
+  background-color: rgb(84, 92, 100);
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .login-card {
+    overflow: hidden;
+    width: 600px;
+    border-radius: 20px;
+    box-shadow: 0px 0px 10px #000;
+    .title {
+      height: 150px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      color: #fff;
+      font-size: 26px;
+    }
+    .content {
+      background-color: #fff;
+      padding: 60px;
+      .svg-container {
+        padding: 6px 5px 6px 15px;
+        color: #889aa4;
+        vertical-align: middle;
+        width: 30px;
+        display: inline-block;
+      }
+      .show-pwd {
+        position: absolute;
+        right: 10px;
+        top: 7px;
+        font-size: 16px;
+        color: #889aa4;
+        cursor: pointer;
+        user-select: none;
+      }
+      .show-code {
+        position: absolute;
+        right: 0px;
+        top: 0px;
+        font-size: 16px;
+        color: #889aa4;
+        cursor: pointer;
+        user-select: none;
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
 /* 修复input 背景不协调 和光标变色 */
 
 $bg:#283443;
-$light_gray:#fff;
-$cursor: #fff;
+$light_gray:#666;
+$cursor: #666;
 
 @supports (-webkit-mask: none) and (not (cater-color: $cursor)) {
   .login-container .el-input input {
@@ -237,7 +289,6 @@ $cursor: #fff;
     display: inline-block;
     height: 47px;
     width: 85%;
-
     input {
       background: transparent;
       border: 0px;
@@ -254,75 +305,14 @@ $cursor: #fff;
       }
     }
   }
-
   .el-form-item {
-    border: 1px solid rgba(255, 255, 255, 0.1);
-    background: rgba(0, 0, 0, 0.1);
+    border: 1px solid rgba(0, 0, 0, 0.1);
+    background: rgba(255, 255, 255, 1);
     border-radius: 5px;
-    color: #454545;
   }
-}
-</style>
-
-<style lang="scss" scoped>
-$bg:#2d3a4b;
-$dark_gray:#889aa4;
-$light_gray:#eee;
-
-.login-container {
-  min-height: 100%;
-  min-width: 767px;
-  width: 100%;
-  background-color: $bg;
-  overflow: hidden;
-
-  .login-form {
-    position: relative;
-    width: 520px;
-    max-width: 100%;
-    padding: 160px 35px 0;
-    margin: 0 auto;
-    overflow: hidden;
-  }
-
-  .svg-container {
-    padding: 6px 5px 6px 15px;
-    color: $dark_gray;
-    vertical-align: middle;
-    width: 30px;
-    display: inline-block;
-  }
-
-  .title-container {
-    position: relative;
-
-    .title {
-      font-size: 26px;
-      color: $light_gray;
-      margin: 0px auto 40px auto;
-      text-align: center;
-      font-weight: bold;
-    }
-  }
-
-  .show-pwd {
-    position: absolute;
-    right: 10px;
-    top: 7px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
-  }
-
-  .show-code {
-    position: absolute;
-    right: -120px;
-    top: 0px;
-    font-size: 16px;
-    color: $dark_gray;
-    cursor: pointer;
-    user-select: none;
+  .el-button {
+    height: 52px;
+    width: 100%;
   }
 }
 </style>
