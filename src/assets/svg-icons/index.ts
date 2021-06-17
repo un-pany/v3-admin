0b21f2c2217497b44355e0507dbae761648911a8
pany <pany@un-net.com>
2021-06-17 11:00:31 +0800

    perf: 关闭组件全局注册

--- src/assets/svg-icons/index.ts
+++ src/assets/svg-icons/index.ts
@@ -14,6 +14,9 @@
 //   app.config.globalProperties.$IconSvg = result.map(e => e.default.id)
 // }
 
+import { createApp } from 'vue'
+import SvgIcon from '@/components/svg-icon/index.vue'// svg component
+
 const requireAll = function(requireContext: any) {
   return requireContext.keys().map(requireContext)
 }
@@ -21,4 +24,6 @@ const req = require.context('./icons', false, /\.svg$/)
 
 requireAll(req)
 
-export default req
+export default function(app: ReturnType<typeof createApp>) {
+  app.component('SvgIcon', SvgIcon)
+}
--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,16 @@ import '@/styles/index.scss'
 import 'normalize.css'
 import * as directives from '@/directives'
 import '@/permission'
-import loadComponents from '@/components'
-import '@/assets/svg-icons'
+// import loadComponents from '@/components'
+import loadSvg from '@/assets/svg-icons'
 
 const app = createApp(App)
 // 加载所有插件
 loadAllPlugins(app)
-
-loadComponents(app)
+// 加载所有全局组件
+// loadComponents(app)
+// 加载全局SVG
+loadSvg(app)
 
 // 自定义指令
 Object.keys(directives).forEach(key => {
