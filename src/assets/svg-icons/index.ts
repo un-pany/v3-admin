35f012f7b7acc996c90cd60b8f96dbd1996e569e
pany <pany@un-net.com>
2021-07-22 16:32:28 +0800

    perf: 精简项目

--- src/assets/svg-icons/index.ts
+++ src/assets/svg-icons/index.ts
@@ -1,19 +1,4 @@
 
-// const requireComponent = require.context(
-//   // 其组件目录的相对路径
-//   './icons',
-//   // 是否查询其子目录
-//   true,
-//   // 匹配基础组件文件名的正则表达式
-//   /\.svg$/
-// )
-
-// const result = requireComponent.keys().map(requireComponent)
-
-// export default function (app: any) {
-//   app.config.globalProperties.$IconSvg = result.map(e => e.default.id)
-// }
-
 import { createApp } from 'vue'
 import SvgIcon from '@/components/svg-icon/index.vue'// svg component
 
