ea6aaaf169963e287d436438ba42ec75e481ab66
pany <pany@un-net.com>
2021-07-29 18:00:53 +0800

    refactor: 移除全局注册组件功能

--- src/main.ts
+++ src/main.ts
@@ -7,17 +7,13 @@ import '@/styles/index.scss'
 import 'normalize.css'
 import * as directives from '@/directives'
 import '@/permission'
-// import loadComponents from '@/components'
 import loadSvg from '@/assets/svg-icons'
 
 const app = createApp(App)
 // 加载所有插件
 loadAllPlugins(app)
-// 加载所有全局组件
-// loadComponents(app)
 // 加载全局SVG
 loadSvg(app)
-
 // 自定义指令
 Object.keys(directives).forEach(key => {
   app.directive(key, (directives as { [key: string]: Directive })[key])
