6a73d139a74f672c4e44af8e26c85c7ea41e54fd
pany <939630029@qq.com>
2021-10-20 14:35:52 +0800

    perf: navigation-bar 组件

--- src/layout/components/navigation-bar/index.vue
+++ src/layout/components/navigation-bar/index.vue
@@ -1,31 +1,21 @@
 <!-- 导航栏 -->
 <template>
   <div class="navbar">
-    <Hamburger
-      id="hamburger-container"
-      :is-active="sidebar.opened"
-      class="hamburger-container"
-      @toggle-click="state.toggleSideBar"
-    />
-    <BreadCrumb id="breadcrumb-container" class="breadcrumb-container" />
+    <Hamburger :is-active="sidebar.opened" class="hamburger" @toggle-click="state.toggleSideBar" />
+    <BreadCrumb class="breadcrumb" />
     <div class="right-menu">
-      <template v-if="device !== 'mobile'">
-        <Screenfull class="right-menu-item hover-effect" v-if="showScreenfull" />
-        <ThemeSwitch class="right-menu-item hover-effect" v-if="showThemeSwitch" />
-      </template>
-      <el-dropdown class="avatar-container right-menu-item hover-effect" trigger="click">
+      <Screenfull class="right-menu-item" v-if="showScreenfull" />
+      <ThemeSwitch class="right-menu-item" v-if="showThemeSwitch" />
+      <el-dropdown class="avatar-container right-menu-item" trigger="click">
         <div class="avatar-wrapper">
           <img src="@/assets/layout/avatar.gif" class="user-avatar">
         </div>
         <template #dropdown>
           <el-dropdown-menu>
-            <router-link to="/">
-              <el-dropdown-item>首页</el-dropdown-item>
-            </router-link>
             <a target="_blank" href="https://juejin.cn/post/6963876125428678693">
               <el-dropdown-item>文档</el-dropdown-item>
             </a>
-            <a target="_blank" href="https://github.com/v3-projects/v3-admin">
+            <a target="_blank" href="https://github.com/un-pany/v3-admin">
               <el-dropdown-item>Github</el-dropdown-item>
             </a>
             <el-dropdown-item divided @click="state.logout">
@@ -51,9 +41,6 @@ const router = useRouter()
 const sidebar = computed(() => {
   return store.state.app.sidebar
 })
-const device = computed(() => {
-  return store.state.app.device.toString()
-})
 const showThemeSwitch = computed(() => {
   return store.state.settings.showThemeSwitch
 })
@@ -77,75 +64,42 @@ const state = reactive({
 .navbar {
   height: 50px;
   overflow: hidden;
-  position: relative;
   background: #fff;
-  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
-
-  .hamburger-container {
+  .hamburger {
     line-height: 50px;
     height: 100%;
     float: left;
     padding: 0 15px;
     cursor: pointer;
-    transition: background 0.3s;
-    -webkit-tap-highlight-color: transparent;
-
     &:hover {
       background: rgba(0, 0, 0, 0.025);
     }
   }
 
-  .breadcrumb-container {
+  .breadcrumb {
     float: left;
   }
 
   .right-menu {
     float: right;
+    margin-right: 8px;
     height: 100%;
-    line-height: 50px;
-
-    &:focus {
-      outline: none;
-    }
-
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #5a5e66;
     .right-menu-item {
-      display: inline-block;
       padding: 0 8px;
-      height: 100%;
-      font-size: 18px;
-      color: #5a5e66;
-      vertical-align: text-bottom;
-
-      &.hover-effect {
-        cursor: pointer;
-        transition: background 0.3s;
-
-        &:hover {
-          background: rgba(0, 0, 0, 0.025);
-        }
-      }
+      cursor: pointer;
     }
-
     .avatar-container {
       .avatar-wrapper {
-        margin-top: 5px;
-        margin-right: 16px;
-        margin-left: 16px;
-        position: relative;
-
+        padding: 0 8px;
+        cursor: pointer;
         .user-avatar {
-          cursor: pointer;
           width: 40px;
           height: 40px;
           border-radius: 10px;
-        }
-
-        .el-icon-caret-bottom {
-          cursor: pointer;
-          position: absolute;
-          right: -20px;
-          top: 25px;
-          font-size: 12px;
         }
       }
     }
