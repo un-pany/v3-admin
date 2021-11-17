95438b600263731477ac6bda1e36016bd702316d
pany <939630029@qq.com>
2021-11-17 17:16:12 +0800

    refactor: screenfull component

--- src/components/screenfull/index.vue
+++ src/components/screenfull/index.vue
@@ -1,47 +1,39 @@
 <!-- 全屏按钮 -->
 
 <template>
-  <div id="screenfull">
-    <div v-if="state.isFullscreen" @click="state.click">
-      <svg-icon name="exit-fullscreen" font-size="20px" />
-    </div>
-    <div @click="state.click" v-else>
-      <svg-icon name="fullscreen" font-size="20px" />
-    </div>
+  <div @click="state.click">
+    <svg-icon :name="state.isFullscreen ? 'exit-fullscreen' : 'fullscreen'" font-size="20px" />
   </div>
 </template>
 
 <script lang="ts" setup>
-import screenfull from 'screenfull'
 import { onBeforeUnmount, onMounted, reactive } from 'vue'
 import { ElMessage } from 'element-plus'
-
-const sf = screenfull
+import screenfull from 'screenfull'
 
 const state = reactive({
   isFullscreen: false,
   click: () => {
-    if (!sf.isEnabled) {
+    if (!screenfull.isEnabled) {
       ElMessage.warning('您的浏览器无法工作')
-      return false
+      return
     }
-    sf.toggle()
+    screenfull.toggle()
+  },
+  change: () => {
+    if (screenfull.isEnabled) {
+      state.isFullscreen = screenfull.isFullscreen
+    }
   }
 })
-const change = () => {
-  if (sf.isEnabled) {
-    state.isFullscreen = sf.isFullscreen
-  }
-}
 onMounted(() => {
-  if (sf.isEnabled) {
-    sf.on('change', change)
+  if (screenfull.isEnabled) {
+    screenfull.on('change', state.change)
   }
 })
-
 onBeforeUnmount(() => {
-  if (sf.isEnabled) {
-    sf.off('change', change)
+  if (screenfull.isEnabled) {
+    screenfull.off('change', state.change)
   }
 })
 </script>
