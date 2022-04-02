6877a360ef00c97f082e4e72bede9e6900b1d8ad
pany <939630029@qq.com>
2022-04-02 17:53:01 +0800

    chore: 适配 pnpm

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,5 @@
 module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
-  ],
-  env: {
-    development: {
-      // babel-plugin-dynamic-import-node plugin only does one thing by converting all import() to require().
-      // This plugin can significantly increase the speed of hot updates, when you have a large number of pages.
-      plugins: [
-        'dynamic-import-node'
-      ]
-    }
-  }
+  ]
 }
